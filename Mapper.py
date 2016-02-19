7d51edf06198eab107bba0d4787d6212c57ae384
jraizes <jlraizes@gmail.com>
2016-02-18 23:57:12 -0800

    Primarily stylistic changes in Mapper.py. One or two design changes.

--- Mapper.py
+++ Mapper.py
@@ -1,4 +1,4 @@
-'''
+"""
 NOTE:	Nothing in here has been tested yet.
 
 		The functions in this file depend on
@@ -10,29 +10,37 @@ NOTE:	Nothing in here has been tested yet.
 
 		The names for the above methods can be
 		modified, they are simply suggestions.
-'''
+"""
 
 # NOTE:	These values are TEMPORARY, pending further investigation
-medium	= 100
-high	= 1000 
-'''
+#notableDose	= 
+#notablePeriod	= 
+
+#mediumDose		=
+#mediumPeriod	=
+
+#highDose		=
+#highPeriod		=
+"""
 The above variables are tunable parameters that control when
-yellow is reached and when Red is reached (see calcRads and 
-supporting functions for further details). They are measured
-in CPM
-'''
+milestone colors are reached (see documentation for further 
+details). 
+"""
 
 	
 def mapData(data):
-	'''
+	"""
 	Segments the path into mini-paths of varying colors
 	(color dependent on radiation levels)
-	'''
 
-	# Handle strange cases
+	Parameters:
+	data (List): A list of parsed log entries
+	"""
+
+	# Handle datasets of a single entry
 	if len(data) == 1:
 		entry = data[0]
-		radColor = calcRads(entry)
+		radColor = calcRadColor(entry)
 		makeMarker(getPoint(entry), radColor)
 
 	while len(data) > 1:
@@ -40,10 +48,10 @@ def mapData(data):
 		path = []
 
 		# Calculate the radiation color of the path
-		radOne = calcRads(data[0])
-		radTwo = calcRads(data[1])
+		radOne = calcRadColor(data[0])
+		radTwo = calcRadColor(data[1])
 
-		radColor = (radOne + radTwo) // 2
+		radColor = avgRadColor(radOne, radTwo)
 
 		# Extract the lat and long and put them on the path
 		path.append(data[0])
@@ -54,7 +62,7 @@ def mapData(data):
 		while len(data) > 1:
 			path.append(data[0])
 
-			if calcRads(data[1]) == radColor:
+			if calcRadColor(data[1]) == radColor:
 				del data[0]
 			else:
 				break
@@ -64,37 +72,70 @@ def mapData(data):
 
 
 def getPoint(entry):
-	'''
+	"""
 	Extracts the latitude and longitude from a data entry
-	'''
+
+	Parameters:
+	entry (List): A single parsed log entry
+	"""
 	return [entry[4], entry[5]]
 
 
+def avgRadColor(radOne, radTwo):
+	"""
+	Averages two radiation colors.
+	Decodes from a 32 bit ARGB color scheme
 
-def calcRads(entry):
-	'''
-	This function is in charge of one of the most important parts:
-	which color do we put on the map?
+	Parameters:
+	radOne (String): A 32 bit ARGB hex string
+	radTwo (String): A 32 bit ARGB hex string
 
-	Uses an ARGB color scheme in accordance with KML
-	'''
-	alpha = calcAlpha(entry[1])
-	red = calcRed(entry[1])
-	green = calcGreen(entry[1])
-	blue = calcBlue(entry[1])
+	Returns an 8 digit hexadecimal ARGB string
+	"""
+	alpha = (int(radOne[:2], 16) + int(radTwo[:2], 16)) // 2
+	alpha = hex(alpha)[2:].zfill(2)
+
+	red = (int(radOne[2:4], 16) + int(radTwo[2:4], 16)) // 2
+	red = hex(red)[2:].zfill(2)
+
+	green = (int(radOne[4:6], 16) + int(radTwo[4:6], 16)) // 2
+	green = hex(green)[2:].zfill(2)
+
+	blue = (int(radOne[6:8], 16) + int(radTwo[6:8], 16)) // 2
+	blue = hex(blue)[2:].zfill(2)
+	
 	return alpha + red + blue + green
 
 
-'''
+def calcRadColor(entry):
+	"""
+	Calculates the color for a given 
+
+	Uses a 32 bit ARGB color scheme in accordance with KML
+
+	Parameters:
+	entry (List): A single parsed log entry
+	"""
+	alpha 	= calcAlpha(int(entry[1]))
+	red 	= calcRed(int(entry[1]))
+	green	= calcGreen(int(entry[1]))
+	blue	= calcBlue(int(entry[1]))
+	return alpha + red + blue + green
+
+
+"""
 Colors range from Green (low radiation) to Red (high radiation),
 with Yellow between them (medium radiation)
-'''
+"""
 
 def calcRed(radlvl):
-	'''
+	"""
 	Calculates the level of Red
 	Returns a 2 digit hexadecimal string
-	'''
+
+	Parameters:
+	radlvl (int): The radiation level in CPM
+	"""
 	if radlvl >= medium:
 		red = 255
 	else:
@@ -104,10 +145,13 @@ def calcRed(radlvl):
 
 
 def calcGreen(radlvl):
-	'''
+	"""
 	Calculates the level of Green.
 	Returns a 2 digit hexadecimal string
-	'''
+
+	Parameters:
+	radlvl (int): The radiation level in CPM
+	"""
 	if radlvl =< medium:
 		green = 255
 	else:
@@ -115,20 +159,28 @@ def calcGreen(radlvl):
 
 	return hex(green)[2:].zfill(2)
 
+
 def calcBlue(radlvl):
-	'''
+	"""
 	Calculates the level of Blue
 	Returns a 2 digit hexadecimal string
-	'''
+
+	Parameters:
+	radlvl (int): The radiation level in CPM
+	"""
 	# No blue in Green, Yellow, or Red
 	blue = 0
 	return hex(blue)[2:].zfill(2)
 
+
 def calcAlpha(radlvl):
-	'''
+	"""
 	Calculates the level of Alpha
 	Returns a 2 digit hexadecimal string
-	'''
+
+	Parameters:
+	radlvl (int): The radiation level in CPM
+	"""
 	# Fully opaque
 	alpha = 255
 	return hex(alpha)[2:].zfill(2)
